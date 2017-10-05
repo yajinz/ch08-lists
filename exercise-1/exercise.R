b4e2dbf6686fa3f99dbaa8509662d0db10631fc1
Ott Toomet <otoomet@uw.edu>
2017-10-05 11:59:56 -0700

    exercise1: removed the hint to component number renamed exercise-2 to exercise-3

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,36 +1,37 @@
 # Exercise 1: Lists
 
-# Create a vector of everything you ate for breakfast
+# 1. Create a vector of everything you ate for breakfast
 
 
-# Create a vector of everything you ate for lunch
+# 2. Create a vector of everything you ate for lunch
 
 
-# Create a list "meals" that has contains your `breakfast` and `lunch`
+## 3. Create a list "meals" that has contains your `breakfast` and `lunch`
+## Ensure the components of your list are named accordingly
 
 
-# Add a "dinner" element to your "meals" list that has what you plan to eat for dinner
+## 4. Add a "dinner" element to your "meals" list that has what you plan to eat for dinner.
+## you may use assignment with dollar or double brackets.
 
 
-# Use dollar notation to extract your `dinner` element from your list
+# 5. Use dollar notation to extract your `dinner` element from your list
 # and save it in a vector called 'dinner'
 
 
-# Use double-bracket notation to extract your `lunch` element from your list
-# and save it in your list as the element at index 5 (no reason)
+# 6. Use double-bracket notation to extract your `lunch` element from your list
 
 
-# Use single-bracket notation to extract your breakfast and lunch from your list
+# 7. Use single-bracket notation to extract your breakfast and lunch from your list
 # and save them to a list called `early.meals`
 
 
 
 ### Bonus ###
-# Create a list that has the number of items you ate for each meal
-# Hint: use the `lappy()` function
+# 8. Create a list that has the number of items you ate for each meal
+# Hint: use a loop or the `sappy()` function
 
 
-# Write a function that adds pizza to every meal
+# 9. Write a function that adds pizza to every meal
 
 
-# Add pizza to every meal!
+# 10. Add pizza to every meal!
